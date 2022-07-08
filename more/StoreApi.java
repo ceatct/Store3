513177c75c20b6a83b8e8874ccb9472486866b4b
ceatct <99398087+ceatct@users.noreply.github.com>
2022-07-08 21:10:38 +0300

    Update StoreApi.java

--- more/StoreApi.java
+++ more/StoreApi.java
@@ -2,15 +2,15 @@ package com.example.store;
 
 import java.util.List;
 
-//import io.reactivex.Single;
+import io.reactivex.Single;
 import retrofit2.http.GET;
 
 public interface StoreApi {
 
- /*   @GET ("/basket/api/v1.0/categorylist")
+    @GET ("/basket/api/v1.0/categorylist")
     Single<List<CategoryTODO>> getCategoryTODO();
 
     @GET ("/basket/api/v1.0/products")
-    Single<List<ProductTODO>> getProductTODO();*/
+    Single<List<ProductTODO>> getProductTODO();
 
 }
